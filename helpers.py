1185e1f94603d186c678bcfe94adcfc95afa020c
Ryan Slattery <18477649+Ryul0rd@users.noreply.github.com>
2021-10-02 22:21:11 -0700

    Add working version of bird fact generator

---
+++ helpers.py
@@ -0,0 +1,57 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import random
+
+
+class TextGenerator:
+    def __init__(self, checkpoint):
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
+
+    def __call__(self, prompt):
+        inputs = self.tokenizer(prompt, return_tensors='pt')['input_ids']
+        prompt_length = len(self.tokenizer.decode(inputs[0]))
+        outputs = self.model.generate(
+            inputs,
+            max_length=500,
+            do_sample=True,
+            temperature=0.8,
+            top_p=0.95,
+            top_k=60,
+            #num_beams=3,
+            no_repeat_ngram_size=8,
+            pad_token_id=50256, # Set pad_token_id to eos_token_id so we don't see it on the CLI
+            )
+        generated = self.tokenizer.decode(outputs[0])[prompt_length:]
+        return generated
+
+
+class PromptGenerator:
+    def __init__(self):
+        self.facts_in_prompt = 5
+        self.max_new_facts = 6
+        self.LEAD = 'I know all there is to know about birds. I will tell you some cool stuff about birds.'
+        self.CORE_FACTS = [
+            'Bird Fact: Did you know that the Sword-billed Hummingbird is the only bird with a bill longer than its body?',
+            'Bird Fact: Did you know that owls cannot swivel their eyes? Instead they move their heads completely around to see straight behind them.',
+            'Bird Fact: Did you know that the only bird with nostrils at the end of its beak is the kiwi? This placement helps it sniff for food, such as worms and insects on the ground.',
+            'Bird Fact: Mockingbirds can imitate many sounds, from a squeaking door to a cat meowing.',
+            'Bird Fact: Chickens that lay brown eggs have red ear lobes. There is a genetic link between the two.',
+            'Bird Fact: Crows have the largest cerebral hemispheres (brains), relative to body size, of any avian family.',
+        ]
+        self.new_facts = []
+
+    def __call__(self):
+        facts = list()
+        facts.extend(self.CORE_FACTS)
+        facts.extend(self.new_facts)
+        facts = random.sample(facts, k=self.facts_in_prompt)
+        random.shuffle(facts)
+        return self.LEAD + '\n' + '\n'.join(facts)
+
+    def set_new_facts(self, new_facts):
+        self.new_facts = new_facts
+
+    def add_new_facts(self, new_facts):
+        self.new_facts.extend(new_facts)
+        if len(new_facts) > self.max_new_facts:
+            self.new_facts = random.sample(self.new_facts, k=self.max_new_facts)
---
+++ run.py
@@ -0,0 +1,46 @@
+from helpers import PromptGenerator, TextGenerator
+from transformers import pipeline
+
+def main():
+    threshold = 0.5 # This is how confident our classifier should need to be for a fact to pass
+
+    #checkpoint = 'gpt2'                    # Uses aprox 1 GB RAM
+    #checkpoint = 'gpt2-large'              # Uses aprox 6 GB RAM
+    checkpoint = 'EleutherAI/gpt-neo-1.3B' # Uses aprox 10 GB RAM
+    #checkpoint = 'EleutherAI/gpt-neo-2.7B' # Uses aprox 20 GB RAM
+
+    prompt_gen = PromptGenerator()
+    text_gen = TextGenerator(checkpoint=checkpoint)
+    classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
+    topic = 'birds'
+
+
+    prompt = prompt_gen()
+    while True:
+        # Create our facts
+        facts = text_gen(prompt).split('\n')
+
+        # Basic cleaning
+        if len(facts) > 0:
+            facts = facts[:-1] # Remove the sometimes incomplete final line. Should really check to see if it's complete instead.
+        else:
+            prompt = prompt_gen()
+            continue
+        facts = list(filter(lambda fact: len(fact) > 20, facts)) # Remove overly short lines
+        facts = list(filter(lambda fact: 'Bird Fact: ' in fact, facts)) # Lines that don't start with 'Bird Fact: ' are generally of low quality
+        facts = list(map(lambda fact: fact.replace('Bird Fact: ', ''), facts)) # Hide 'Bird Fact: ' from our classifier
+
+        # Checking to see if our facts are about birds
+        facts = classifier(facts, topic)
+        facts = list(filter(lambda fact: fact[0]['scores'][0] > threshold, facts))
+        facts = list(map(lambda fact: fact[0]['sequence'], facts))
+
+        # Printing results and getting our next prompt
+        facts = list(map(lambda fact: 'Bird Fact: ' + fact, facts))
+        for fact in facts:
+            print(fact)
+        prompt_gen.add_new_facts(new_facts=facts)
+        prompt = prompt_gen()
+
+if __name__ == '__main__':
+    main()
